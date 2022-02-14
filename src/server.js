70ef9e32c6b683768d93a579869ab5871733303e
James Davies <60436624+jimdavies72@users.noreply.github.com>
2022-02-14 14:08:16 +0000

    added dotenv

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 5001;
+const port = process.env.HTTP_PORT;
 
-app.use("/static/orders", express.static("./public/orders.html"));
 app.use("/profile", express.static("./public/profile.html"));
+app.use("/static/orders", express.static("./public/orders.html"));
 app.use("/static", express.static("public"));
 app.use("/static/products", express.static("./public/products.html"));
 
