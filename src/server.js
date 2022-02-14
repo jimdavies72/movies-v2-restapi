b80791f67dbe5f40ef6ffe0b2f538015cc4d7982
James Davies <60436624+jimdavies72@users.noreply.github.com>
2022-02-14 13:40:00 +0000

    added orders

--- src/server.js
+++ src/server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const port = 5001;
 
+app.use("/static/orders", express.static("./public/orders.html"));
+app.use("/profile", express.static("./public/profile.html"));
 app.use("/static", express.static("public"));
 app.use("/static/products", express.static("./public/products.html"));
-app.use("/profile", express.static("./public/profile.html"));
 
 app.listen(port, () => {
   console.log(`App is listening on port: ${port}`);
